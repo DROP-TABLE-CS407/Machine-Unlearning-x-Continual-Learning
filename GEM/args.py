0c1d6c57c570732e8533918bd7f2bb6532a8843a
Gerald Ramos <Gerald.Ramos@warwick.ac.uk>
2024-12-02 05:33:45 +0000

    add performance metrics

--- GEM/args.py
+++ GEM/args.py
@@ -13,7 +13,7 @@ class Args:
 
         # optimizer parameters
         self.n_epochs = 10
-        self.batch_size = 10
+        self.batch_size = 1000
         self.lr = 0.1
 
         # experiment parameters nor do we use this stuff 
