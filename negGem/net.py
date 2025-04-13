c5bb5a43b98231416a756d60fd444c4c6ed09079
Krishi <Krishi.Mistry@warwick.ac.uk>
2025-04-13 01:25:55 +0100

    Added ALT-GEM and ALT-NEGGEM unlearning algo - NEEDS TESTING

--- negGem/net.py
+++ negGem/net.py
@@ -192,6 +192,17 @@ class Net(nn.Module):
                 # copy gradients back
                 overwrite_grad(self.parameters, self.grads[:, t],
                             self.grad_dims)
+            elif algorithm == "ALT-GEM":
+                # enforces that learning the new task is guarenteed to be learnt
+                store_grad(self.parameters, self.grads, self.grad_dims, t)
+                indx = torch.cuda.LongTensor(self.observed_tasks) if self.gpu \
+                    else torch.LongTensor(self.observed_tasks)
+                project2cone2(self.grads[:, t].unsqueeze(1),
+                            self.grads.index_select(1, indx), self.margin)
+                # copy gradients back
+                overwrite_grad(self.parameters, self.grads[:, t],
+                            self.grad_dims)
+                
         self.opt.step()
 
 
@@ -420,7 +431,74 @@ class Net(nn.Module):
                 overwrite_grad(self.parameters, self.grads[:, t],
                             self.grad_dims)
             self.opt.step()
+        elif algorithm == "ALT-NEGGEM":
+            ## otherwise we need to unlearn the task 
+            ## we compute the gradients of all learnt tasks
+            current_grad = []
             
+            for param in self.parameters():
+                if param.grad is not None:
+                    current_grad.append(param.grad.data.view(-1))
+            current_grad = torch.cat(current_grad).unsqueeze(1)
+            
+            # now find the grads of the previous tasks
+            for tt in self.observed_tasks:
+                self.zero_grad()
+                past_task = tt
+                offset1, offset2 = compute_offsets(past_task, self.nc_per_task,
+                                                    self.is_cifar)
+                ptloss = self.ce(
+                        self.forward(
+                            self.memory_data[past_task],
+                            past_task)[:, offset1: offset2],
+                        self.memory_labs[past_task] - offset1)
+                if tt == t:
+                    ptloss = self.ce(
+                        self.forward(
+                            self.memory_data[past_task][x1:x2],
+                            past_task)[:, offset1: offset2],
+                        self.memory_labs[past_task][x1:x2] - offset1)
+                    
+                ptloss.backward()
+                store_grad(self.parameters, self.grads, self.grad_dims,
+                            past_task)
+            ## so now, we have the gradients of all the tasks we can now do our projection,
+            ## first we check if it is even neccessary to do so, if not simply do a optimiser.step()
+            
+            self.zero_grad()
+            loss = self.ce(
+                        self.forward(self.memory_data[t][x1:x2], t)[:, offset1: offset2], self.memory_labs[t][x1:x2] - offset1)
+            # negate loss for unlearning
+            loss = -1 * loss
+            loss.backward()
+            
+            store_grad(self.parameters, self.grads, self.grad_dims, t)
+            indx = torch.cuda.LongTensor(self.observed_tasks[:-1]) if self.gpu \
+                else torch.LongTensor(self.observed_tasks[:-1])
+                
+            forget_grads = self.grads[:, t].unsqueeze(1)
+            retain_indices = torch.tensor([i for i in range(self.grads.size(1)) if i in self.observed_tasks and i != t], device=self.grads.device)
+            retain_grads = self.grads.index_select(1, retain_indices)
+
+            all_indices = torch.tensor([i for i in range(self.grads.size(1)) if i in self.observed_tasks], device=self.grads.device)
+            all_grads = self.grads.index_select(1, all_indices)
+
+            average_grad_of_retain = torch.mean(retain_grads, dim=1, keepdim=True)
+
+            dotp = torch.mm(self.grads[:, t].unsqueeze(0),
+                                self.grads.index_select(1, indx))
+            if (dotp < 0).sum() != 0:
+                print("Projection needed")
+                if self.salun:
+                    apply_salun(average_grad_of_retain, self.salun_threshold)
+                NegAGEM(average_grad_of_retain, all_grads, self.unlearn_memory_strength)
+                self.grads[:, t] = forget_grads.squeeze(1)
+                # copy gradients back
+                overwrite_grad(self.parameters, self.grads[:, t],
+                            self.grad_dims)
+            
+            self.opt.step()
+
         else:
             print("Invalid Algorithm")
             
