bb8f3f0425d3bf7f253564107705d15c8dfd9fa1
Gerald Mark Louise Ramos <126425575+tapchonk@users.noreply.github.com>
2025-04-19 23:30:59 +0100

    SANITY CHECK

--- negGem/net.py
+++ negGem/net.py
@@ -172,6 +172,11 @@ class Net(nn.Module):
         # now compute the grad on the current minibatch
         self.zero_grad()
 
+        # for all grads NOT in self.observed tasks, set to 0
+        for tt in range(20):
+            if tt not in self.observed_tasks:
+                self.grads[:, tt].fill_(0.0)
+
         offset1, offset2 = compute_offsets(t, self.nc_per_task, self.is_cifar) 
         loss = self.ce(self.forward(x, t)[:, offset1: offset2], y - offset1)
         loss.backward()
